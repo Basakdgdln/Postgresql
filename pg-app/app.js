dc9dbcf36d0d5953e7896eca46a919046d9adb11
Basakdgddln <basakdagdelenn@gmail.com>
2024-03-10 20:16:53 +0300

    postgresql completed.

---
+++ pg-app/app.js
@@ -0,0 +1,50 @@
+// node app.js ifadesini çalıştırırken veriitabanının arkada çalışmadığından emin ol.
+"use strict";
+const express = require("express");
+const dbConnection = require("./postgresql");
+
+const app = express();
+
+dbConnection.connect((err, client) => {
+    if (err) {
+      console.error("Database connection error: ", err.stack);
+    } else {
+      console.log("Connected to PostgreSQL database");
+    }
+});
+  
+app.get("/students/add/:name/:puan", (req, res) => {
+  dbConnection.query(
+    `INSERT INTO student (name, puan) VALUES ('${req.params.name}', ${parseInt(req.params.puan)})`,
+    (err, results, fields) => {
+      if (err) {
+        console.log("Database query error: ", err);
+      }
+      else {
+        console.log("Data inserted succesfully");
+      }
+    });
+});
+
+
+app.get("/students", (req, res) => {
+  dbConnection.query("SELECT * FROM student", (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      });
+    }
+  });
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+
+
+
+
+
---
+++ pg-app/postgre.js
@@ -0,0 +1,10 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  host: "localhost", //localhost
+  user: "user",
+  port: "5432",
+  password: "example",
+  database: "app",
+});
+module.exports = pool;
+
